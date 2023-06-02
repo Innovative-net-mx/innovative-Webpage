185206cd2703a8d4dc28f77676035a6f268f7747
Daniel Jara <danieljc1111@gmail.com>
2023-06-02 11:31:19 -0700

    contact view and model

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Formulario_Contacto():
+    nombre = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.PhoneNumberField()
+    empresa = models.CharField()
+    extension = models.IntegerField()
+    descripcion = models.CharField()
+
--- homepage/views.py
+++ homepage/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
-
+from .models import *
 from django.http import HttpResponse
-from django.shortcuts import render
+
 # Create your views here.
 def index(request):
     return render(request, "homepage/index.html")
 
 def template(request):
     return render(request, "homepage/template.html")
+
+#form model view for contact form
+def contact(request):
+    if request.method == "POST":
+        nombre = request.POST.get('nombre')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        empresa = request.POST.get('empresa')
+        extension = request.POST.get('extension')
+        descripcion = request.POST.get('descripcion')
+        contact = Formulario_Contacto(nombre=nombre, email=email, phone=phone, empresa=empresa, extension=extension, descripcion=descripcion)
+        contact.save()
+        return redirect('index')
+    return render(request, "homepage/index.html")
