736911f04f8e47cfb1e0a2b246cbcff798c0bef6
Daniel Jara <danieljc1111@gmail.com>
2023-06-19 10:50:51 -0700

    Se agrego clase a contacto form

---
+++ homepage/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Formulario_Contacto
+
+
+class FormularioContactoForm(forms.ModelForm):
+    class Meta:
+        model = Formulario_Contacto
+        fields = ['nombre', 'email', 'phone',
+                  'empresa', 'extension', 'descripcion']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'contact-from'}),
+            'email': forms.EmailInput(attrs={'class': 'contact-from'}),
+            'phone': forms.NumberInput(attrs={'class': 'contact-from-phone'}),
+            'empresa': forms.TextInput(attrs={'class': 'contact-from'}),
+            'extension': forms.NumberInput(attrs={'class': 'contact-from-extension'}),
+            'descripcion': forms.TextInput(attrs={'class': 'contact-from-descripcion'}),
+        }
--- homepage/views.py
+++ homepage/views.py
@@ -3,6 +3,7 @@ from django.views.generic import FormView
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
+from .forms import *
 
 # Create your views here.
 from .models import *
@@ -22,6 +23,12 @@ def inicio(request):
 # make a class view for a form Formulario_Contacto
 class Contacto_Form(CreateView):
     model = Formulario_Contacto
-    fields = ['nombre', 'email', 'phone', 'empresa', 'extension', 'descripcion']
-    success_url = reverse_lazy('homepage:home')
+    form_class = FormularioContactoForm 
     template_name = 'homepage/contacto.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('index')
