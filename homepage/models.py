eecd67ba32e1e2dda148fa50cc49e34cc0389078
Daniel Jara <danieljc1111@gmail.com>
2023-07-10 11:37:50 -0700

    cambio en el modelo formulario_contacto

--- homepage/models.py
+++ homepage/models.py
@@ -7,8 +7,8 @@ from django.db import models
 class Formulario_Contacto(models.Model):
     nombre = models.CharField(max_length=50)
     email = models.EmailField()
-    phone = models.IntegerField()
-    empresa = models.CharField(max_length=50)
-    extension = models.IntegerField()
+    phone = models.IntegerField(max_length=11)
+    empresa = models.CharField(max_length=60)
+    extension = models.IntegerField(blank=True, null=True, max_length=5)
     descripcion = models.CharField(max_length=200)
 
