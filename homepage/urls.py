0a8d97848716974312b1616fa3c3eed0ad106356
Daniel Jara <danieljc1111@gmail.com>
2023-07-18 11:07:37 -0700

    se agrego noticia listado & crear

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import Contacto_Form
+from .views import CRM_noticias_list, Contacto_Form, CRM_noticias_create
 urlpatterns = [ 
     path("", views.index, name="index"),
     path("cartera_servicios", views.cartera_servicios, name="cartera_servicios"),
@@ -8,5 +8,7 @@ urlpatterns = [
     path("objetivos", views.objetivos, name="objetivos" ),
     path("inicio",   views.inicio, name="inicio"),
     path("error_form", views.error_form, name="error_form"),
+    path("crm_noticias_list", CRM_noticias_list.as_view(), name="crm_noticias_list"),
+    path("crm_noticias_create", CRM_noticias_create.as_view(), name="crm_noticias_create"),
     
 ]
