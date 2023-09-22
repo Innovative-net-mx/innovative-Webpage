a8f6e2c37557b68379f7189646b6be2802f784bb
daniel.jara <danieljc1111@gmail.com>
2023-09-21 17:33:08 -0700

    / in media

--- innovative_net/settings.py
+++ innovative_net/settings.py
@@ -130,5 +130,5 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
